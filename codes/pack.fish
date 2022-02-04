8e52db5432eec1eccabeaebab85b661c48d55021
tsingkwai <tsingkwai@bluelaketable>
2022-02-04 10:24:52 +0800

    2022-02-04_02:24:52

--- codes/pack.fish
+++ codes/pack.fish
@@ -1,12 +1,14 @@
 function pack
     set -lx resource_dir $argv[1]
+    set -lx recudir (pwd)
     if [ "$argv[1]" = "" ]
         set resource_dir .
     end
-    set package_level (sed -n '/package_level=/'p $resource_dir/ctpm_pkg_info | sed 's/package_level=//g')
-    set package_name (sed -n '/package_name=/'p $resource_dir/ctpm_pkg_info | sed 's/package_name=//g')
-    set package_ver (sed -n '/package_ver=/'p $resource_dir/ctpm_pkg_info | sed 's/package_ver=//g')
-    set package_unis (sed -n '/package_unis=/'p $resource_dir/ctpm_pkg_info | sed 's/package_unis=//g')
+    cd $resource_dir
+    set package_level (sed -n '/package_level=/'p ctpm_pkg_info | sed 's/package_level=//g')
+    set package_name (sed -n '/package_name=/'p ctpm_pkg_info | sed 's/package_name=//g')
+    set package_ver (sed -n '/package_ver=/'p ctpm_pkg_info | sed 's/package_ver=//g')
+    set package_unis (sed -n '/package_unis=/'p ctpm_pkg_info | sed 's/package_unis=//g')
     if [ "$package_name" = "" ]
         logger 4 'No package_name defined,abort'
         exit
@@ -19,23 +21,25 @@ function pack
         logger 4 'No package_level defined,abort'
         exit
     end
-    if test -d $resource_dir/src
-        if test -e $resource_dir/src/file_list
-            for src_file in (cat $resource_dir/src/file_list)
-                if test -e $resource_dir/src$src_file
+    if test -d src
+        if test -e src/file_list
+            for src_file in (cat src/file_list)
+                if test -e src$src_file
                 else
-                    logger 4 "$src_file doesn't exist in $resource_dir/src,but you declared it in file_list,abort"
+                    logger 4 "$src_file doesn't exist in src,but you declared it in file_list,abort"
                     exit
                 end
             end
         else
-            logger 4 "No $resource_dir/src/file_list defined,abort"
+            logger 4 "No src/file_list defined,abort"
             exit
         end
     else
         logger 4 'No src directory,abort'
     end
     logger 0 "Packing $package_name.ctpkg"
-    tar zcf $package_name.ctpkg $resource_dir/ &>/dev/null
-    logger 0 "Processed,store at $package_name.ctpkg"
+    tar zcf $package_name.ctpkg . &>/dev/null
+    mv $package_name.ctpkg $recudir
+    cd $recudir
+    logger 0 "Processed,store at $recudir/$package_name.ctpkg"
 end
